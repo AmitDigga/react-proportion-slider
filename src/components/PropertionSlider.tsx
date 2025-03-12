000c767c0e2b15cc8e8a3d7d4ae812c8b26608b8
Amit Digga <diggaamitoo7@gmail.com>
2025-03-12 13:20:45 +0530

    create DynamicChildPositioner for the required animations

--- src/components/PropertionSlider.tsx
+++ src/components/PropertionSlider.tsx
@@ -1,4 +1,10 @@
-import { useCallback, useEffect, useRef } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
 export type ProportionDetail = {
   name: string;
@@ -127,40 +133,38 @@ export const Proportion = ({
   displayValueType,
   backgroundColor = "gray",
 }: ProportionProp) => {
-  const percent = (value * 100) / total;
-  const percentFormatted = `${Math.round(percent)}%`;
-  return (
+  const nameNode = (
     <div
       style={{
-        width,
-        background: backgroundColor,
-        alignSelf: "stretch",
-        borderRadius: "5px",
-        color: "white",
-        display: "flex",
-        flexDirection: anchorName === "left" ? "row" : "row-reverse",
-        alignItems: "center",
+        userSelect: "none",
+        whiteSpace: "nowrap",
       }}
     >
+      {detail.name}
+    </div>
+  );
+  const percentNode =
+    displayValueType === "percentage" ? (
       <div
         style={{
-          marginLeft: anchorName === "left" ? "5px" : "auto",
-          marginRight: anchorName === "right" ? "5px" : "auto",
           userSelect: "none",
+          whiteSpace: "nowrap",
         }}
       >
-        {detail.name}
+        {`${Math.round((value * 100) / total)}%`}
       </div>
-      {displayValueType === "percentage" ? (
-        <div
-          style={{
-            userSelect: "none",
-          }}
-        >
-          {percentFormatted}
-        </div>
-      ) : null}
-    </div>
+    ) : null;
+
+  return (
+    <DynamicChildPositioner
+      width={width}
+      rightNode={anchorName === "right" ? nameNode : percentNode}
+      leftNode={anchorName === "left" ? nameNode : percentNode}
+      options={{
+        primary: anchorName,
+      }}
+      backgroundColor={backgroundColor}
+    ></DynamicChildPositioner>
   );
 };
 
@@ -228,5 +232,155 @@ export const SliderKnob = ({
         cursor: "ew-resize",
       }}
     ></div>
+  );
+};
+
+// AnimatableTextContainer animates text outside of component if
+// its longer than the container. It uses CSS animations to move.
+// There will be two text in each. One in the left and one in the right.
+// If the width is not enough to show the text, it will animate the text
+// to show the full text. It will move the text outside of boundary of container
+
+const GAP = 5;
+const STYLES = {
+  TOP_LEFT: {
+    left: GAP,
+    top: -GAP,
+    transform: "translateY(-100%)",
+  },
+  LEFT: {
+    left: GAP,
+    top: "50%",
+    transform: "translateY(-50%)",
+  },
+  BOTTOM_LEFT: {
+    left: GAP,
+    bottom: -GAP,
+    transform: "translateY(100%)",
+  },
+  TOP_RIGHT: {
+    right: GAP,
+    top: -GAP,
+    transform: "translateY(-100%)",
+  },
+  RIGHT: {
+    right: GAP,
+    top: "50%",
+    transform: "translateY(-50%)",
+  },
+  BOTTOM_RIGHT: {
+    right: GAP,
+    bottom: -GAP,
+    transform: "translateY(100%)",
+  },
+};
+
+type DynamicChildPositionerProps = {
+  rightNode: React.ReactNode;
+  leftNode: React.ReactNode;
+  options: {
+    primary: "left" | "right";
+  };
+  backgroundColor?: string;
+  width: number | string;
+};
+const DynamicChildPositioner = ({
+  rightNode,
+  leftNode,
+  options: { primary },
+  width,
+  backgroundColor = "gray",
+}: DynamicChildPositionerProps) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const refRight = useRef<HTMLDivElement | null>(null);
+  const refLeft = useRef<HTMLDivElement | null>(null);
+  const [fitStatus, setFitStatus] = useState<"both" | "primary" | "none">(
+    "both"
+  );
+
+  const rightStyle = useMemo(() => {
+    const rightNoSpace =
+      fitStatus === "none" || (fitStatus === "primary" && primary === "left");
+    if (primary === "right") {
+      return rightNoSpace ? STYLES["BOTTOM_RIGHT"] : STYLES["RIGHT"];
+    } else {
+      return rightNoSpace ? STYLES["TOP_LEFT"] : STYLES["RIGHT"];
+    }
+  }, [fitStatus, primary]);
+
+  const leftStyle = useMemo(() => {
+    const leftNoSpace =
+      fitStatus === "none" || (fitStatus === "primary" && primary === "right");
+    if (primary === "left") {
+      return leftNoSpace ? STYLES["BOTTOM_LEFT"] : STYLES["LEFT"];
+    } else {
+      return leftNoSpace ? STYLES["TOP_RIGHT"] : STYLES["LEFT"];
+    }
+  }, [fitStatus, primary]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const textRight = refRight.current;
+      const textLeft = refLeft.current;
+      const container = ref.current;
+      if (!container || !textRight || !textLeft) {
+        return;
+      }
+      const containerRectWidth = container.getBoundingClientRect().width;
+      const rightWidth = textRight.getBoundingClientRect().width;
+      const leftWidth = textLeft.getBoundingClientRect().width;
+
+      const secondaryWidth = primary === "left" ? leftWidth : rightWidth;
+      const primaryWidth = primary === "left" ? rightWidth : leftWidth;
+
+      const primaryCanFit = primaryWidth + 2 * GAP <= containerRectWidth;
+      const secondaryCanFit =
+        secondaryWidth + primaryWidth + 3 * GAP <= containerRectWidth;
+
+      const fitStatus = primaryCanFit
+        ? secondaryCanFit
+          ? "both"
+          : "primary"
+        : "none";
+
+      setFitStatus(fitStatus);
+    }, 1000 / 30);
+    return () => clearInterval(interval);
+  }, [ref, refRight, refLeft, primary]);
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        position: "relative",
+        width,
+        backgroundColor,
+        borderRadius: "5px",
+        color: "white",
+      }}
+    >
+      <div
+        ref={refLeft}
+        style={{
+          position: "absolute",
+          transition:
+            "transform 0.25s cubic-bezier(.47,1.64,.41,.8), top 0.25s cubic-bezier(.47,1.64,.41,.8)",
+          ...leftStyle,
+        }}
+      >
+        {leftNode}
+      </div>
+      <div
+        ref={refRight}
+        style={{
+          position: "absolute",
+          transition:
+            "transform 0.25s cubic-bezier(.47,1.64,.41,.8), top 0.25s cubic-bezier(.47,1.64,.41,.8)",
+          ...rightStyle,
+        }}
+      >
+        {rightNode}
+      </div>
+    </div>
   );
 };
